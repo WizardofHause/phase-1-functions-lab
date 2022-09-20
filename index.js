ceb265dbb1a226528cd53b5a9eadcb27ddad388a
Jon Hause <jonrhause@gmail.com>
2022-09-19 22:16:52 -0600

    commit tree 68219e0c4f827e690e2e5b36825f222f4bb4a550

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    return (block >= 42 ? block-42 : 42-block);
+}
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block)*264; 
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const howFar = (start >= destination ? start-destination : destination-start)*264;
+    return howFar;
+}
+
+function calculatesFarePrice(start, destination) {
+    if ((distanceTravelledInFeet(start, destination)) <= 400) {
+        return 0;
+    } else if ((distanceTravelledInFeet(start, destination)) > 400 && (distanceTravelledInFeet(start, destination)) <= 2000) {
+        return (distanceTravelledInFeet(start, destination)-400)*0.02;
+    } else if ((distanceTravelledInFeet(start, destination)) > 2000 && (distanceTravelledInFeet(start, destination)) <= 2500) {
+        return 25;
+    } else if ((distanceTravelledInFeet(start, destination)) > 2500) {
+        return `cannot travel that far`;
+    }
+}
